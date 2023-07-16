4ac60c4ccaa8c087ab659e368296868ad6d7afda
Omar <omar@atumm.org>
2023-07-16 08:29:45 +0000

    remove unused import

--- buti/core.py
+++ buti/core.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from collections import UserDict
 from enum import Enum
-from typing import Any, Dict, List, Optional, overload
+from typing import Any, Dict, List, Optional
 
 
 class ButiKeys(Enum):
