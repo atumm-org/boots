511beccb82f38c434abbe09891068a1826a5a74d
Omar <omar@atumm.org>
2023-07-26 14:00:43 +0000

    fix repository url

--- buti/__init__.py
+++ buti/__init__.py
@@ -2,6 +2,13 @@
 
 __author__ = """Omars"""
 __email__ = "omar@atumm.org"
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
-from .core import *
+from .core import BootableComponent, Bootloader, ButiKeys, ButiStore
+
+__all__ = [
+    "Bootloader",
+    "BootableComponent",
+    "ButiKeys",
+    "ButiStore",
+]
